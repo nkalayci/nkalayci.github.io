0e835dfe8ce5ec07addf52e65b3031c85df2e838
nekalayc <34338225+nekalayc@users.noreply.github.com>
2021-03-11 13:47:55 +0300

    func

---
+++ ServiceBusTopicTrigger1/ServiceBusTopicTrigger2/run.ps1
@@ -0,0 +1,8 @@
+
+param($QueueItem, $TriggerMetadata) 
+
+Push-OutputBinding -Name outputSbMsg -Value @{ 
+    name = $QueueItem.name 
+    employeeId = $QueueItem.employeeId 
+    address = $QueueItem.address 
+} 
